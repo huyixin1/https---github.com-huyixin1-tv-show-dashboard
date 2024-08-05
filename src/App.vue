ae40dc71f8b05877027b24990315e041fda31be5
yixin <44286866+huyixin1@users.noreply.github.com>
2024-08-05 15:55:53 +0200

    add search button to the search bars

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,45 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+  
+    <header>
+      <h1>TV Show Dashboard</h1>
+      <SearchBar @search="handleSearch" />
+    </header>
+    <h1>{{ message }}</h1>
+    <router-view :key="$route.fullPath" />
+    
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios';
+import SearchBar from './components/SearchBar.vue';
 
 export default {
-  name: 'App',
+  data() {
+    return {
+      message: null
+    };
+    },
   components: {
-    HelloWorld
+    SearchBar
+  },
+  methods: {
+    async handleSearch(query) {
+      try {
+        const response = await axios.get(`http://api.tvmaze.com/search/shows?q=${query}`);
+        if (response.data.length > 0) {
+          const show = response.data[0].show; // Get the first show in the results
+          this.$router.push(`/show/${show.id}`);
+        } else {
+          this.message = "There is no show";
+        }
+      } catch (error) {
+        console.error('Error searching for show:', error);
+      }
+    }
   }
-}
+};
 </script>
 
 <style>
@@ -22,5 +50,16 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+header {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-bottom: 20px;
+}
+
+h1 {
+  margin: 0;
 }
 </style>
---
+++ src/components/SearchBar.vue
@@ -0,0 +1,37 @@
+<template>
+  <input type="text" v-model="query" @input="search" placeholder="Search for TV shows..." />
+  <button @click="search">Search</button>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      query: ''
+    };
+  },
+  methods: {
+    search() {
+      this.$emit('search', this.query);
+    }
+  }
+};
+</script>
+
+<style scoped>
+div {
+  display: flex;
+  align-items: center;
+}
+
+input {
+  flex: 1;
+  margin-right: 10px;
+  padding: 5px;
+}
+
+button {
+  padding: 5px 10px;
+  cursor: pointer;
+}
+</style>
